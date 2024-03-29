f4cc4cb3fdf80143b327438be03f779ec166992c
Jana Šimečková <jana.simeckova@thermofisher.com>
2024-03-29 23:05:04 +0100

    last little changes - basic first version done

--- Player.gd
+++ Player.gd
@@ -6,7 +6,6 @@ const MAXFALLSPEED = 2000
 const GRAVITY = 1000
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var motion = Vector2.ZERO
 var wall : PackedScene = preload("res://wallnode.tscn")
@@ -20,31 +19,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("FLAP"):
 		motion.y = FLAP
 		
-	self.velocity = motion # Set the body's velocity property
-	self.move_and_slide() # Call move_and_slide with no parameters
+	self.velocity = motion 
+	self.move_and_slide() 
 	
 	get_parent().get_parent().get_node("CanvasLayer/RichTextLabel").text = str(score)
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
-
+	
 func Wall_reset():
 	var wall_inst = wall.instantiate()
-	wall_inst.position = Vector2(3000, randi_range(-2500, 2500))
+	wall_inst.position = Vector2(3000, randi_range(-1500, 1500))
 	get_parent().call_deferred("add_child", wall_inst)
 	
 func _on_resetter_body_entered(body):
--- Walls.gd
+++ Walls.gd
@@ -1,9 +1,7 @@
 extends StaticBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 func _physics_process(delta):
 	position += Vector2(-10,0)
