03ce454494464651fd7d88a4c76d436e91809d9c
Petr Jelínek <petr.jelinek@greencode.cz>
2024-04-07 11:37:44 +0200

    Add stats for counting semesters and space hits

--- Player.gd
+++ Player.gd
@@ -9,7 +9,10 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var motion = Vector2.ZERO
 var wall: PackedScene = preload("res://wallnode.tscn")
-var score = 0 
+
+var credits = 0
+var semester_counter = 1
+var space_hit = 0
 
 func increase_wall_speed(increase_amount: float) -> void:
 	Global.wall_speed -= increase_amount
@@ -21,17 +24,19 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("FLAP"):
 		motion.y = FLAP
+		space_hit += 1
 		
 	self.velocity = motion 
 	self.move_and_slide() 
 	
-	get_parent().get_parent().get_node("CanvasLayer/ScoreText").text = "Credits - " + str(score)
+	get_parent().get_parent().get_node("CanvasLayer/ScoreText").text = "Credits - " + str(credits)
 	
 func generate_new_level():
 	var wall_inst = wall.instantiate()
 	wall_inst.position = Vector2(3000, randi_range(-1000, 1000))
 	get_parent().call_deferred("add_child", wall_inst)
 	increase_wall_speed(10)
+	semester_counter += 1
 
 # Detect "player passed all walls in semester successully"
 # and new walls (level) needs to be generated
@@ -42,11 +47,17 @@ func _on_resetter_body_entered(body):
 
 # Detect collission player - wall
 func _on_detect_body_entered(body):
-	if body.name.contains("Walls"):     
-		get_tree().reload_current_scene()
-		Global.wall_speed = -15
+	if body.name.contains("Walls"):   
+		# TODO Display game over screen with these stats
+		# print(space_hit) 
+		# print(semester_counter)
+		reset_game()
 
 # Add credit when player successfully leaves the "wall area"
 func _on_detect_area_exited(area):
 	if area.name == "PointArea":
-		score += 1
+		credits += 1
+
+func reset_game():
+	get_tree().reload_current_scene()
+	Global.wall_speed = -15
